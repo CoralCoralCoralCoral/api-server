041655f97f48ef1c31dac47b42e46fc38413e654
Stan <111stan35@gmail.com>
2024-11-15 20:38:56 +0000

    Initial Commit 2

---
+++ src/main/java/com/coral/epidemicsimapiserver/EpidemicSimApiServerApplication.java
@@ -0,0 +1,13 @@
+package com.coral.epidemicsimapiserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EpidemicSimApiServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EpidemicSimApiServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/coral/epidemicsimapiserver/EpidemicSimApiServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coral.epidemicsimapiserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EpidemicSimApiServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
