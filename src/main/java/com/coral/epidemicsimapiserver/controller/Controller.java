8881451cbd28142cb44e3d26813016c11c39507c
Stan <111stan35@gmail.com>
2024-11-15 21:46:01 +0000

    Added simple / route and html

---
+++ src/main/java/com/coral/epidemicsimapiserver/controller/Controller.java
@@ -0,0 +1,12 @@
+package com.coral.epidemicsimapiserver.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+
+@org.springframework.stereotype.Controller
+public class Controller {
+
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+}
