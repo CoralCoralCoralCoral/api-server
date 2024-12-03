820651ff528825a38acc074b62b0191e21f8c8d3
Umran Hussain <1547890+umran@users.noreply.github.com>
2024-12-03 23:14:25 +0000

    use default spring rabbit config

--- src/main/java/com/coral/epidemicsimapiserver/configuration/RabbitMQConfig.java
+++ src/main/java/com/coral/epidemicsimapiserver/configuration/RabbitMQConfig.java
@@ -3,25 +3,17 @@ package com.coral.epidemicsimapiserver.configuration;
 import com.rabbitmq.client.ConnectionFactory;
 import org.springframework.amqp.core.*;
 import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.autoconfigure.amqp.RabbitProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-
 @Configuration
 public class RabbitMQConfig {
-
-    @Value("${RMQ_URI}")
-    private String RMQ_URI;
-
     public static final String GAME_UPDATE_QUEUE_NAME = "game_update";
     private static final String GAME_UPDATE_EXCHANGE_NAME = "game-updates";
     private static final String GAME_UPDATE_ROUTING_KEY = "test";
 
     private static final String INIT_GAME_QUEUE_NAME = "init_game";
     private static final String INIT_GAME_EXCHANGE_NAME = "init-game";
-
 
     @Bean
     public Queue queue() {
@@ -55,17 +47,7 @@ public class RabbitMQConfig {
 
     @Bean
     public ConnectionFactory connectionFactory() {
-        System.out.println("THIS IS THE RMQ_URI:" + RMQ_URI);
         CachingConnectionFactory connectionFactory = new CachingConnectionFactory();
-        connectionFactory.setUri(RMQ_URI); // Use the URI provided in application.properties
         return connectionFactory.getRabbitConnectionFactory();
     }
-
-    @Bean
-    public RabbitProperties rabbitProperties() {
-        RabbitProperties properties = new RabbitProperties();
-        properties.setAddresses(RMQ_URI);
-        return properties;
-    }
-
 }
