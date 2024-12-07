dc98ac8f963212218e1dcb0c4c59916761905f9f
Umran Hussain <1547890+umran@users.noreply.github.com>
2024-12-07 20:22:56 +0000

    fixed

--- src/main/java/com/coral/epidemicsimapiserver/controller/Controller.java
+++ src/main/java/com/coral/epidemicsimapiserver/controller/Controller.java
@@ -2,8 +2,8 @@ package com.coral.epidemicsimapiserver.controller;
 
 import com.coral.epidemicsimapiserver.EpidemicSimApiServerApplication;
 import com.coral.epidemicsimapiserver.configuration.RabbitMQConfig;
-import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
@@ -12,6 +12,8 @@ import org.springframework.web.servlet.view.RedirectView;
 
 @org.springframework.stereotype.Controller
 public class Controller {
+    @Autowired
+    private RabbitTemplate rabbitTemplate;
 
     @GetMapping("/")
     public RedirectView index() {
@@ -21,8 +23,7 @@ public class Controller {
     @MessageMapping("/game/{id}/command")
     public void forwardCommands(@Payload String payload, @DestinationVariable("id") String id) {
         System.out.println(id + "---" + payload);
-        RabbitTemplate rabbitTemplate = new RabbitTemplate();
-        rabbitTemplate.setConnectionFactory(new CachingConnectionFactory());
+
         rabbitTemplate.convertAndSend(
                 RabbitMQConfig.GAME_COMMAND_EXCHANGE_NAME,
                 EpidemicSimApiServerApplication.SERVER_UUID + "." + id,
