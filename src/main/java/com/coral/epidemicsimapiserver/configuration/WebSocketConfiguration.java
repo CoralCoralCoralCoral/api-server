cc6e2866fd41de6b1f76cee83fdabf1b6d2a62ab
Umran Hussain <1547890+umran@users.noreply.github.com>
2024-12-08 01:17:55 +0000

    open up websockets cors to all hosts

--- src/main/java/com/coral/epidemicsimapiserver/configuration/WebSocketConfiguration.java
+++ src/main/java/com/coral/epidemicsimapiserver/configuration/WebSocketConfiguration.java
@@ -16,7 +16,7 @@ public class WebSocketConfiguration implements WebSocketMessageBrokerConfigurer 
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/websocket").setAllowedOrigins("http://localhost:3000", "http://127.0.0.1:3000").withSockJS();
-        registry.addEndpoint("/websocket").setAllowedOrigins("http://localhost:3000", "http://127.0.0.1:3000");
+        registry.addEndpoint("/websocket").setAllowedOrigins("*").withSockJS();
+        registry.addEndpoint("/websocket").setAllowedOrigins("*");
     }
 }
